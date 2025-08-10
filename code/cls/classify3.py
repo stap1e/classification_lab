29a54ae3cd023a84672387048ed46e1a7ab809ee
4tap1e <liuhongyu162@gmail.com>
2025-08-10 16:16:14 +0800

    nse

--- code/cls/classify3.py
+++ code/cls/classify3.py
@@ -151,31 +151,33 @@ def main():
     print(f"数据划分完毕")
 
     # 初始化分类器
-    clf = GaussianNB(priors=[0.83, 0.17])    # priors=[0.5, 0.5]
+    ratio0 = len([x for x in data_train['label'].tolist() if x == 0]) / len(data_train['label'].tolist())
+    ratio1 = len([x for x in data_train['label'].tolist() if x == 1]) / len(data_train['label'].tolist())
+    clf = GaussianNB(priors=[ratio0, ratio1])    # priors=[0.5, 0.5]
     clf = LogisticRegression()
 
     lgbm_params = {
-        'objective': 'binary',  # 二分类任务
+        'objective': 'binary',       # 二分类任务
         'metric': 'binary_logloss',  # 使用logloss作为评价指标
         'learning_rate': 0.016,
         'max_depth': 6,
         'n_estimators': 500,
-        'subsample': 0.7,  # 构建每棵树时使用的样本比例
-        'colsample_bytree': 0.7,  # 每棵树使用的特征比例
+        'subsample': 0.7,            # 构建每棵树时使用的样本比例
+        'colsample_bytree': 0.7,     # 每棵树使用的特征比例
         'scale_pos_weight': scale_pos_weight,  # 根据数据不平衡调整正负样本的权重
         'random_state': 42,
     }
     clf = LGBMClassifier(**lgbm_params)
 
     catboost_params = {
-        'iterations': 500,  # 迭代次数
-        'depth': 6,  # 树的深度
-        'learning_rate': 0.01,  # 学习率
+        'iterations': 500,           # 迭代次数
+        'depth': 6,                  # 树的深度
+        'learning_rate': 0.01,       # 学习率
         'loss_function': 'Logloss',  # 损失函数
-        'eval_metric': 'AUC',  # AUC作为评价指标
+        'eval_metric': 'AUC',        # AUC作为评价指标
         'scale_pos_weight': scale_pos_weight,  # 样本不平衡的调整
         'random_seed': 42,
-        'verbose': 0  # 不输出训练过程
+        'verbose': 0                 # 不输出训练过程
     }
     clf = CatBoostClassifier(**catboost_params)
 
@@ -184,11 +186,11 @@ def main():
         'learning_rate': 0.016,
         'max_depth': 6,
         'n_estimators': 600,
-        'subsample': 0.72,         # 用于构建每棵树的样本比例
-        'colsample_bytree': 0.705,  # 控制每棵树在构建时使用的特征比例
+        'subsample': 0.72,           # 用于构建每棵树的样本比例
+        'colsample_bytree': 0.705,   # 控制每棵树在构建时使用的特征比例
         # 'scale_pos_weight': 1.4,   # 根据实际正负样本比例设置权重 len(y[y==0]) / len(y[y==1]),
         'gamma': 0.1,
-        'min_child_weight': 1,     # 降低以增加模型灵活性
+        'min_child_weight': 1,       # 降低以增加模型灵活性
         'scale_pos_weight': scale_pos_weight,
         'random_state': 42,
         # 'tree_method': 'hist',  
@@ -202,9 +204,7 @@ def main():
         gamma='auto',              # scale
         probability=True,          # 启用概率估计
         class_weight='balanced',   # 处理类别不平衡 
-        random_state=42)    
-                                                    
-    random_var = [42, 46, 52]
+        random_state=42)  
 
     # 参数网格
     param_grid = {
